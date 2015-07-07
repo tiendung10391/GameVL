a066f11252a4043139b33da458d3fd8163001e83
Nguyễn Tiến Dũng <tiendung10391@gmail.com>
2015-07-08 00:20:28 +0700

    tro giup dap an

--- src/itpsoft/app/gamevl/chonDapAn.java
+++ src/itpsoft/app/gamevl/chonDapAn.java
@@ -5,6 +5,7 @@ import itpsoft.app.gamevl.entity.chonDapAnEntity;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 import android.app.Activity;
@@ -15,16 +16,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class chonDapAn extends Activity {
 	private CountDownTimer countDownTimer;
 	private boolean timerHasStarted = false;
-	public TextView tvTime, tvCauHoi;
-	public ImageView imvCau1, imvCau2, imvCau3, imvCau4;
+	private TextView tvTime, tvCauHoi;
+	private ImageView imvCau1, imvCau2, imvCau3, imvCau4;
+	private ImageView imvNext, imv50, imvX2;
+	private RelativeLayout layoutCau1, layoutCau2, layoutCau3, layoutCau4;
 	private LinearLayout m_main;
 	private int numberQuestion = 1;
 	private TextView tvNumerQuestion;
@@ -39,6 +48,9 @@ public class chonDapAn extends Activity {
 	private ArrayList<String> AnserAfterRandom;
 	private int Diem = 0;
 	private int ThoiGian = 0;
+	private boolean checkTroGiupX2 = false;
+	private boolean checkCloseDialogDiemCao = false;
+	private boolean check50 = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +62,7 @@ public class chonDapAn extends Activity {
 		getQuestion();
 		countDownTime();
 		eventAnsers();
+		eventSupport();
 
 	}
 
@@ -61,6 +74,13 @@ public class chonDapAn extends Activity {
 		imvCau3 = (ImageView) findViewById(R.id.imvCau3);
 		imvCau4 = (ImageView) findViewById(R.id.imvCau4);
 		tvNumerQuestion = (TextView) findViewById(R.id.tvNumberQuestion);
+		imvNext = (ImageView) findViewById(R.id.imvNext);
+		imvX2 = (ImageView) findViewById(R.id.imvx2);
+		imv50 = (ImageView) findViewById(R.id.imv50);
+		layoutCau1 = (RelativeLayout) findViewById(R.id.layoutCau1);
+		layoutCau2 = (RelativeLayout) findViewById(R.id.layoutCau2);
+		layoutCau3 = (RelativeLayout) findViewById(R.id.layoutCau3);
+		layoutCau4 = (RelativeLayout) findViewById(R.id.layoutCau4);
 
 		db = new DatabaseHandler(this);
 
@@ -68,6 +88,14 @@ public class chonDapAn extends Activity {
 
 	public void getQuestion() {
 		try {
+			setDefaltBackGroundDapAn();
+			if (check50) {
+				layoutCau1.setVisibility(View.VISIBLE);
+				layoutCau2.setVisibility(View.VISIBLE);
+				layoutCau3.setVisibility(View.VISIBLE);
+				layoutCau4.setVisibility(View.VISIBLE);
+				check50 = false;
+			}
 			// random cau hoi
 			arrQuetion = db.getAllChonCauHoi();
 			int randomId = getIDQuestion(arrQuetion.size());
@@ -110,11 +138,118 @@ public class chonDapAn extends Activity {
 			// set numberQuestion
 			String NumberQuestion = "Câu " + String.valueOf(numberQuestion);
 			tvNumerQuestion.setText(NumberQuestion);
-			numberQuestion++;
+			// numberQuestion++;
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+
+	}
+
+	public void eventSupport() {
+		imvNext.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				countDownTimer.cancel();
+				countDownTimer.start();
+				getQuestion();
+				checkTroGiupX2 = false;
+				AlphaAnimation fade_out = new AlphaAnimation(1.0f, 0.0f);
+				fade_out.setDuration(500);
+				fade_out.setAnimationListener(new AnimationListener() {
+					public void onAnimationStart(Animation arg0) {
+					}
+
+					public void onAnimationRepeat(Animation arg0) {
+					}
+
+					public void onAnimationEnd(Animation arg0) {
+						imvNext.setVisibility(View.GONE);
+					}
+				});
+				imvNext.startAnimation(fade_out);
+
+			}
+		});
+
+		imv50.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				AlphaAnimation fade_out = new AlphaAnimation(1.0f, 0.0f);
+				fade_out.setDuration(500);
+				fade_out.setAnimationListener(new AnimationListener() {
+					public void onAnimationStart(Animation arg0) {
+					}
+
+					public void onAnimationRepeat(Animation arg0) {
+					}
+
+					public void onAnimationEnd(Animation arg0) {
+						imv50.setVisibility(View.GONE);
+					}
+				});
+				imv50.startAnimation(fade_out);
+
+				// code
+				ArrayList<Integer> arrDapAnSai = new ArrayList<Integer>();
+				for (int i = 0; i < 4; i++) {
+					if (!AnserAfterRandom.get(i).endsWith(DapAnDung)) {
+						arrDapAnSai.add(i);
+					}
+				}
+
+				Collections.shuffle(arrDapAnSai);
+
+				for (int j = 0; j < 2; j++) {
+					int loai = arrDapAnSai.get(j);
+					switch (loai) {
+					case 0:
+						layoutCau1.setVisibility(View.GONE);
+						break;
+					case 1:
+						layoutCau2.setVisibility(View.GONE);
+						break;
+					case 2:
+						layoutCau3.setVisibility(View.GONE);
+						break;
+					case 3:
+						layoutCau4.setVisibility(View.GONE);
+						break;
+					default:
+						break;
+					}
+				}
+
+				check50 = true;
+
+			}
+		});
+
+		imvX2.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				checkTroGiupX2 = true;
+				AlphaAnimation fade_out = new AlphaAnimation(1.0f, 0.0f);
+				fade_out.setDuration(500);
+				fade_out.setAnimationListener(new AnimationListener() {
+					public void onAnimationStart(Animation arg0) {
+					}
+
+					public void onAnimationRepeat(Animation arg0) {
+					}
+
+					public void onAnimationEnd(Animation arg0) {
+						imvX2.setVisibility(View.GONE);
+					}
+				});
+				imvX2.startAnimation(fade_out);
+			}
+		});
 
 	}
 
@@ -123,8 +258,17 @@ public class chonDapAn extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				checkAnser(0);
-
+				if (checkCloseDialogDiemCao) {
+					checkCloseDialogDiemCao = false;
+					restartTimeAndQuestion();
+					getQuestion();
+					dialogStart();
+				} else {
+					if (!checkAnser(0)) {
+						layoutCau1
+								.setBackgroundResource(R.drawable.background_dapan_false);
+					}
+				}
 			}
 		});
 
@@ -132,8 +276,17 @@ public class chonDapAn extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				checkAnser(1);
+				if (checkCloseDialogDiemCao) {
+					checkCloseDialogDiemCao = false;
+					restartTimeAndQuestion();
+					getQuestion();
+					dialogStart();
+				} else {
+					if (!checkAnser(1)) {
+						layoutCau2
+								.setBackgroundResource(R.drawable.background_dapan_false);
+					}
+				}
 			}
 		});
 
@@ -141,8 +294,17 @@ public class chonDapAn extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				checkAnser(2);
+				if (checkCloseDialogDiemCao) {
+					checkCloseDialogDiemCao = false;
+					restartTimeAndQuestion();
+					getQuestion();
+					dialogStart();
+				} else {
+					if (!checkAnser(2)) {
+						layoutCau3
+								.setBackgroundResource(R.drawable.background_dapan_false);
+					}
+				}
 			}
 		});
 
@@ -150,42 +312,124 @@ public class chonDapAn extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				checkAnser(3);
+				if (checkCloseDialogDiemCao) {
+					checkCloseDialogDiemCao = false;
+					restartTimeAndQuestion();
+					getQuestion();
+					dialogStart();
+				} else {
+					if (!checkAnser(3)) {
+						layoutCau4
+								.setBackgroundResource(R.drawable.background_dapan_false);
+					}
+
+				}
 			}
 		});
 	}
 
 	// kiem tra dap an
-	public void checkAnser(int i) {
+	public boolean checkAnser(int i) {
 		if (AnserAfterRandom.get(i).equals(DapAnDung)) {
 			Diem++;
+			countDownTimer.cancel();
+			countDownTimer.start();
+			numberQuestion++;
 			getQuestion();
+			checkTroGiupX2 = false;
+			return true;
 		} else {
-			dialogDiemCao();
-			Toast.makeText(getApplicationContext(), "Sai roi",
-					Toast.LENGTH_SHORT).show();
+			if (checkTroGiupX2) {
+				checkTroGiupX2 = false;
+				return false;
+			} else {
+				for (int j = 0; j < 4; j++) {
+					if (AnserAfterRandom.get(j).equals(DapAnDung)) {
+						switch (j) {
+						case 0:
+							layoutCau1
+									.setBackgroundResource(R.drawable.background_dapan_true);
+							break;
+						case 1:
+							layoutCau2
+									.setBackgroundResource(R.drawable.background_dapan_true);
+							break;
+						case 2:
+							layoutCau3
+									.setBackgroundResource(R.drawable.background_dapan_true);
+							break;
+						case 3:
+							layoutCau4
+									.setBackgroundResource(R.drawable.background_dapan_true);
+							break;
+
+						default:
+							break;
+						}
+					}
+				}
+				dialogDiemCao();
+
+				return false;
+			}
 		}
 	}
 
 	public void dialogDiemCao() {
-		final Dialog dialog = new Dialog(chonDapAn.this, R.style.Dialog_No_Border);
+		final Dialog dialog = new Dialog(chonDapAn.this,
+				R.style.Dialog_No_Border);
 		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		LayoutInflater inflater = LayoutInflater.from(chonDapAn.this);
 		View view = inflater.inflate(R.layout.diemcao_chon_dap_an, null);
 		m_main = (LinearLayout) view.findViewById(R.id.diemCaoGame1);
 		m_main.setBackgroundResource(R.drawable.style_border_dialog);
-		ImageView tvStart = (ImageView) view.findViewById(R.id.imvCloseDiemCaoGame1);
-		tvStart.setOnClickListener(new OnClickListener() {
+		ImageView btClose = (ImageView) view
+				.findViewById(R.id.imvCloseDiemCaoGame1);
+		Button btChoiTiep = (Button) view.findViewById(R.id.btChoiTiepGame1);
+		Button btThoat = (Button) view.findViewById(R.id.btThoatGame1);
+		TextView tvDiem = (TextView) view.findViewById(R.id.tvDiemGame1);
+		TextView tvThoiGian = (TextView) view
+				.findViewById(R.id.tvThoiGianGame1);
+		tvDiem.setText(String.valueOf(Diem));
+		tvThoiGian.setText(String.valueOf(ThoiGian) + "s");
+
+		btClose.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				dialog.dismiss();
 				dialog.getWindow().getAttributes().windowAnimations = R.style.PauseDialogAnimation;
+				checkCloseDialogDiemCao = true;
 			}
 		});
+
+		btThoat.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();
+				dialog.getWindow().getAttributes().windowAnimations = R.style.PauseDialogAnimation;
+				finish();
+				overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+			}
+		});
+
+		btChoiTiep.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				countDownTimer.start();
+				restartTimeAndQuestion();
+				dialog.dismiss();
+				dialog.getWindow().getAttributes().windowAnimations = R.style.PauseDialogAnimation;
+			}
+		});
+
 		dialog.setContentView(view);
 		dialog.show();
+
+		countDownTimer.cancel();
 
 		dialog.getWindow().getAttributes().windowAnimations = R.style.PauseDialogAnimation;
 	}
@@ -213,6 +457,23 @@ public class chonDapAn extends Activity {
 		dialog.getWindow().getAttributes().windowAnimations = R.style.PauseDialogAnimation;
 	}
 
+	public void setDefaltBackGroundDapAn() {
+		layoutCau1.setBackgroundResource(R.drawable.background_dapan);
+		layoutCau2.setBackgroundResource(R.drawable.background_dapan);
+		layoutCau3.setBackgroundResource(R.drawable.background_dapan);
+		layoutCau4.setBackgroundResource(R.drawable.background_dapan);
+	}
+
+	public void restartTimeAndQuestion() {
+		Diem = 0;
+		ThoiGian = 0;
+		numberQuestion = 1;
+		imvNext.setVisibility(View.VISIBLE);
+		imv50.setVisibility(View.VISIBLE);
+		imvX2.setVisibility(View.VISIBLE);
+		getQuestion();
+	}
+
 	public void countDownTime() {
 		countDownTimer = new MyCountDownTimer(startTime, interval);
 		tvTime.setText(tvTime.getText() + String.valueOf(startTime / 1000));
@@ -227,17 +488,14 @@ public class chonDapAn extends Activity {
 		@Override
 		public void onFinish() {
 			// code
-			countDownTimer.start();
-			Toast.makeText(getApplicationContext(), "chuyen cau",
-					Toast.LENGTH_SHORT).show();
-			getQuestion();
+			dialogDiemCao();
 
 		}
 
 		@Override
 		public void onTick(long millisUntilFinished) {
 			tvTime.setText("" + millisUntilFinished / 1000);
-
+			ThoiGian++;
 		}
 	}
 
